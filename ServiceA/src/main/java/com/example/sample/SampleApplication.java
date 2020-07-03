cd432fad02fef0527575931aae2dbf51df8bdb9a
Murat Karakas <murat.karakas@eteration.com>
2020-07-03 09:34:36 +0300

    version bump

--- ServiceA/src/main/java/com/example/sample/SampleApplication.java
+++ ServiceA/src/main/java/com/example/sample/SampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @RestController
@@ -17,6 +17,8 @@ public class SampleApplication {
 	private RestTemplate restTemplate;
 
 	public static void main(String[] args) {
+		//ElasticApmAttacher.attach();
+
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
